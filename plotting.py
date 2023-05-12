c099e205a4e41d7c72c661548c4297c513e1b622
Radha Patel <rrpatel@mit.edu>
2023-05-12 17:01:19 -0400

    Remove unneeded helpers

--- plotting.py
+++ plotting.py
@@ -24,22 +24,6 @@ class Plotting:
         self.plot_path(path)
         plt.show()
 
-    def animation_lrta(self, path, visited, name):
-        self.plot_grid(name)
-        cl = self.color_list_2()
-        path_combine = []
-
-        for k in range(len(path)):
-            self.plot_visited(visited[k], cl[k])
-            plt.pause(0.2)
-            self.plot_path(path[k])
-            path_combine += path[k]
-            plt.pause(0.2)
-        if self.xI in path_combine:
-            path_combine.remove(self.xI)
-        self.plot_path(path_combine)
-        plt.show()
-
     def animation_ara_star(self, path, visited, name):
         self.plot_grid(name)
         cl_v, cl_p = self.color_list()
@@ -73,12 +57,6 @@ class Plotting:
             self.plot_path(path[k], cl_p[k], True)
             plt.pause(0.5)
 
-        plt.show()
-
-    def animation_bi_astar(self, path, v_fore, v_back, name):
-        self.plot_grid(name)
-        self.plot_visited_bi(v_fore, v_back)
-        self.plot_path(path)
         plt.show()
 
     def plot_grid(self, name):
@@ -133,28 +111,6 @@ class Plotting:
         plt.plot(self.xI[0], self.xI[1], "bs")
         plt.plot(self.xG[0], self.xG[1], "gs")
 
-        plt.pause(0.01)
-
-    def plot_visited_bi(self, v_fore, v_back):
-        if self.xI in v_fore:
-            v_fore.remove(self.xI)
-
-        if self.xG in v_back:
-            v_back.remove(self.xG)
-
-        len_fore, len_back = len(v_fore), len(v_back)
-
-        for k in range(max(len_fore, len_back)):
-            if k < len_fore:
-                plt.plot(v_fore[k][0], v_fore[k][1], linewidth='3', color='gray', marker='o')
-            if k < len_back:
-                plt.plot(v_back[k][0], v_back[k][1], linewidth='3', color='cornflowerblue', marker='o')
-
-            plt.gcf().canvas.mpl_connect('key_release_event',
-                                         lambda event: [exit(0) if event.key == 'escape' else None])
-
-            if k % 10 == 0:
-                plt.pause(0.001)
         plt.pause(0.01)
 
     @staticmethod
